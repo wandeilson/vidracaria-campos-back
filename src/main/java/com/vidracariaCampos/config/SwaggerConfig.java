db3516edf70b65e1cbfcdc4032daa4a67cd59790
Ezequias Soares <87997012+EzequiasSoares1@users.noreply.github.com>
2023-12-05 21:04:49 -0300

    update documentation

--- src/main/java/com/vidracariaCampos/config/SwaggerConfig.java
+++ src/main/java/com/vidracariaCampos/config/SwaggerConfig.java
@@ -1,6 +1,10 @@
 package com.vidracariaCampos.config;
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,7 +16,17 @@ public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("Your API").version("1.0.0"));
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
+                        )
+                )
+                .info(new Info().title("Your API").version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 
     @Override
@@ -22,4 +36,3 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .resourceChain(false);
     }
 }
-
