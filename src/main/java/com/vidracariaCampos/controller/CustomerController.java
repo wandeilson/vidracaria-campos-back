bc287d5b52459ce75721a33d55a335527efd6575
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-12-07 20:41:42 -0300

    Finalizing basic customer CRUD

--- src/main/java/com/vidracariaCampos/controller/CustomerController.java
+++ src/main/java/com/vidracariaCampos/controller/CustomerController.java
@@ -4,10 +4,11 @@ import com.vidracariaCampos.entity.Customer;
 import com.vidracariaCampos.service.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/customers")
@@ -20,6 +21,54 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<Customer> create (@RequestBody Customer customer){
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(customer));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        return ResponseEntity.ok(customerService.getAllCustomers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable (value = "id") UUID id){
+        Optional<Customer> customerOptional = customerService.findById(id);
+        if(!customerOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(customerOptional.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteCustomerById (@PathVariable (value = "id") UUID id){
+        Optional<Customer> optionalCustomer = customerService.findById(id);
+        if(!optionalCustomer.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found.");
+        }
+        customerService.delete(optionalCustomer.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Customer deleted successfully.");
 
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateCustomer(@PathVariable (value = "id") UUID id,
+                                                    @RequestBody Customer customer){
+        Optional<Customer> customerOptional = customerService.findById(id);
+        if(!customerOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found");
+        }
+
+        customer.setId(customerOptional.get().getId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.save(customer));
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/vidracariaCampos/service/CustomerService.java
+++ src/main/java/com/vidracariaCampos/service/CustomerService.java
@@ -4,6 +4,11 @@ import com.vidracariaCampos.entity.Customer;
 import com.vidracariaCampos.repository.CustomerRepositoty;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class CustomerService {
 
@@ -13,6 +18,19 @@ public class CustomerService {
         this.customerRepositoty = customerRepositoty;
     }
     public Customer save(Customer customer) {
+        customer.setRegistrationDate(LocalDateTime.now());
         return customerRepositoty.save(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepositoty.findAll();
+    }
+
+    public Optional<Customer> findById(UUID id) {
+        return customerRepositoty.findById(id);
+    }
+
+    public void delete(Customer customer) {
+        customerRepositoty.delete(customer);
     }
 }
