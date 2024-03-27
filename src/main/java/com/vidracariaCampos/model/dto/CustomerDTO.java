6c3a685c847abb7d04ddaa857bedb63e1027d389
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2024-03-27 19:48:39 -0300

    Modifica validacoes do CustomerDTO

--- src/main/java/com/vidracariaCampos/model/dto/CustomerDTO.java
+++ src/main/java/com/vidracariaCampos/model/dto/CustomerDTO.java
@@ -4,6 +4,7 @@ import com.vidracariaCampos.model.entity.Address;
 import com.vidracariaCampos.model.enums.CustomerType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 
@@ -11,12 +12,13 @@ public record CustomerDTO(
         @NotBlank(message = "Name cannot be blank")
         @Size(min = 5, message = "Name must contain at least 5 characters")
         String name,
+        @NotNull(message = "Customer type cannot be blank")
         CustomerType customerType,
         String cpfcnpj,
         @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
         String email,
         @NotBlank
-        @Size(min = 10, message = "Phone must contain as least 10 characters")
+        @Size(min = 8, message = "Phone must contain as least 10 characters")
         String phone,
         Address address
 
