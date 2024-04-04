e20d45829f069c0baae21c68806f5bdfe5b66295
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2024-04-04 00:26:38 -0300

    Adiciona attr registrationDate

--- src/main/java/com/vidracariaCampos/model/dto/ProductCreateDTO.java
+++ src/main/java/com/vidracariaCampos/model/dto/ProductCreateDTO.java
@@ -4,10 +4,13 @@ import com.vidracariaCampos.model.enums.Category;
 import com.vidracariaCampos.model.enums.UnitOfMeasure;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDateTime;
+
 public record ProductCreateDTO(
         @NotNull String name,
         @NotNull UnitOfMeasure unitOfMeasure,
-        @NotNull Category category
+        @NotNull Category category,
+        LocalDateTime registrationDate
 
 ){
 
