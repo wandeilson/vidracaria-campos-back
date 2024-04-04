00431e2c404abfb4f95b2fc795944f2c75ff3a8d
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2024-04-03 23:19:47 -0300

    Mudança de hierarquia de pasta

---
+++ src/main/java/com/vidracariaCampos/repository/CustomerRepositoty.java
@@ -0,0 +1,29 @@
+package com.vidracariaCampos.repository;
+
+import com.vidracariaCampos.model.entity.Customer;
+import com.vidracariaCampos.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+@Repository
+public interface CustomerRepositoty extends JpaRepository<Customer, UUID> {
+
+    boolean existsByEmailAndIdUser(String email, UUID idUser);
+    boolean existsByCpfcnpjAndIdUser(String cpf_cnpj, UUID idUser);
+
+    @Query("SELECT c FROM Customer c WHERE c.idUser = :idUser")
+    List<Customer> findCustomersByUserId(@Param("idUser") UUID idUser);
+
+    boolean existsByIdAndIdUser(UUID id, UUID idUser);
+
+    @Transactional
+    void deleteByIdAndIdUser(UUID uuid, UUID idUser);
+
+    Optional<Customer> findByIdAndIdUser(UUID id, UUID idUser);
+}
